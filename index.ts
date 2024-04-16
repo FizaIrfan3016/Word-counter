99fe8ee4d2677e183fa275642fe28b0318cff28c
Fiza Irfan <159923486+FizaIrfan3016@users.noreply.github.com>
2024-04-16 12:04:56 -0700

    Here is my word counter code.

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+//Import inquirer module.
+import inquirer from "inquirer";
+
+const answers : {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+      name:"Sentence",
+      type:"input",
+      message:"Enter your sentence to count the word:"
+    }
+])
+// using trim to clear white space and using split to convert the number of elements in array. 
+//.trim and .split is a method.
+const words = answers.Sentence.trim().split(" ")
+
+//print the array of words to the console
+console.log(words)
+
+//print the word count of the sentence to the console.
+console.log(`Your sentence word count is ${words.length}`);
